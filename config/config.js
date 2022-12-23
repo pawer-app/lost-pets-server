24126b491deddc8421847361f2c7fdfa44781796
Sandro Gomez Araya <sandro.gomez@gmail.com>
2022-12-23 20:26:45 -0300

    temp: trying to migrate

--- config/config.js
+++ config/config.js
@@ -6,12 +6,8 @@ console.log(`is usging SSL > ${isUsingSSL}`)
 
 module.exports = {
   [environment]: {
+    url: process.env.PSQL_URL,
     dialect: "postgres",
-    username: "doadmin",
-    password: process.env.SEQ_DATABASE_PASSWORD,
-    database: "pawerorg_db",
-    port: 25060,
-    host: "pawer-postgres-do-user-10028346-0.b.db.ondigitalocean.com",
     dialectOptions: {
       ssl:
         isUsingSSL ? 
