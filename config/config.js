63ccc22c15f00c9c12c498d3da6e03f750490d23
Sandro Gomez Araya <sandro.gomez@gmail.com>
2022-12-23 20:13:29 -0300

    fix: dbuser on PG

--- config/config.js
+++ config/config.js
@@ -6,10 +6,10 @@ console.log(`is usging SSL`);
 module.exports = {
   [environment]: {
     dialect: "postgres",
-    username: "pawerorg_usr",
+    username: "doadmin",
     password: process.env.SEQ_DATABASE_PASSWORD,
     database: "pawerorg_db",
-    port: 25061,
+    port: 25060,
     host: "pawer-postgres-do-user-10028346-0.b.db.ondigitalocean.com",
     ssl: true,
     dialectOptions: {
